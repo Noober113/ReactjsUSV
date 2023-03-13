6cbcbaa1d58a71539e6a7de939459fdc6a99d22a
Noober113 <9a5.27.phuc@gmail.com>
2023-03-13 23:50:26 +0700

    R in CRUD

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,82 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUser } from '../../services/userService';
+
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUser('ALL');
+        // console.log('get all user', response.data.users)
+        if (response && response.data.errCode === 0) {
+            this.setState({
+                arrUsers: response.data.users
+            })
+        }
     }
 
 
     render() {
+        // console.log('1', this.state)
+        let arrUser = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div>
+                <section>
+                    {/* <!--for demo wrap--> */}
+                    <h1>Manage user table</h1>
+                    <div className="tbl-header">
+                        <table cellPadding="0" cellSpacing="0" border="0">
+                            <thead>
+                                <tr>
+                                    <th>Email</th>
+                                    <th>Fullname</th>
+                                    <th>Address</th>
+                                    <th>Phonenumber</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div className="tbl-content">
+                        <table cellpadding="0" cellspacing="0" border="0">
+                            <tbody>
+                                {arrUser && arrUser.map((item, index) => {
+                                    console.log('checkmap', item, index)
+                                    return (
+                                        <tr key={index}>
+                                            <td>{item.email}</td>
+                                            <td>{item.fullName}</td>
+                                            <td>{item.address}</td>
+                                            <td>{item.phoneNumber}</td>
+                                            <td>
+                                                <div id="container">
+                                                    <button type="submit" id="edit-button">
+                                                        <i className="far fa-edit"></i>
+                                                    </button>
+                                                    <button type="submit" id="end-editing">
+                                                        <i className="fas fa-trash-alt"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                                }
+
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,4 +5,8 @@ const handleLogin = (email, password) => {
     return axios.post('http://localhost:8080/api/login', { email, password });
 }
 
-export { handleLogin }
+const getAllUser = (inputId) => {
+    return axios.get(`http://localhost:8080/api/get-all-users?id=${inputId}`);
+}
+
+export { handleLogin, getAllUser }
